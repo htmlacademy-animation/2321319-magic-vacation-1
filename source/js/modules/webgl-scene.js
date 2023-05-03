2f346560e4f4e59089f44f9904168cf91310f44e
tadian-solo <tadian-solo@yandex.ru>
2023-05-03 16:45:17 +0300

    Анимируем мигание интенсивности эффекта Hue

--- source/js/modules/webgl-scene.js
+++ source/js/modules/webgl-scene.js
@@ -1,13 +1,15 @@
 import * as THREE from "three";
 import CustomMaterial from "./3d/custom-material";
+import {Screen} from "../general/consts";
 
 export default class WebGLScene {
-  constructor(canvasElement, sceneObjects) {
+  constructor(canvasElement, storySettings) {
     this.canvas = canvasElement;
     this.canvas.width = window.innerWidth;
     this.canvas.height = window.innerHeight;
     this.aspectRatio = this.canvas.width / this.canvas.height;
-    this.sceneObjects = sceneObjects;
+    this.sceneObjects = {};
+    this.initSceneObjects(storySettings);
     this.alpha = 1;
     this.backgroundColor = `#5f458c`;
     this.near = 0.1;
@@ -21,8 +23,74 @@ export default class WebGLScene {
     };
     this.isLoading = true;
     this.currentSceneObject = null;
+
+    this.frameInterval = 1000 / 50;
+    this.runnungAnimation = null;
+    this.startTime = null;
+    this.lastFrameTime = null;
+
     window.addEventListener(`resize`, () => this.setSizes());
     this.init();
+  }
+
+  initSceneObjects(storySettings) {
+    const imageWidth = 2048;
+    const imageHeight = 1024;
+    this.sceneObjects[Screen.TOP] = {
+      url: `./img/module-5/scenes-textures/scene-0.png`,
+      position: {
+        x: 0,
+        y: 0,
+        z: 0,
+        width: imageWidth,
+        height: imageHeight
+      },
+      hue: 0.0,
+      bubbles: [],
+      durations: [],
+      finites: [],
+      delays: [],
+      animationFunctions: []
+    };
+    Object.entries(storySettings).forEach(([key, value], index) => {
+      this.sceneObjects[key] = {
+        url: value.backgroundImage,
+        position: {
+          x: imageWidth * (index + 1),
+          y: 0,
+          z: 0,
+          width: imageWidth,
+          height: imageHeight
+        },
+        hue: index === 1 ? -12.0 : 0.0,
+        // нормализованные координаты
+        bubbles: index === 1
+          ? [
+            {
+              center: { x: 0.45, y: 0.75 },
+              radius: 0.08,
+            },
+            {
+              center: { x: 0.3, y: 0.6 },
+              radius: 0.06,
+            },
+            {
+              center: { x: 0.49, y: 0.4 },
+              radius: 0.03,
+            }
+          ]
+          : [],
+        durations: [1800, 600],
+        finites: [false, true],
+        delays: [0, 200],
+        animationFunctions: index === 1
+          ? [
+            (progress) => this.hueBlinkAnimationFunct(progress),
+            (progress) => this.bubblesMoveAnimationFunct(progress)
+          ]
+          : [],
+      };
+    });
   }
 
   init() {
@@ -67,6 +135,7 @@ export default class WebGLScene {
       results.forEach((result, i) => {
         if (result.status === `fulfilled`) {
           const objectSettings = Object.values(this.sceneObjects);
+          const key = Object.keys(this.sceneObjects)[i];
           const material = new CustomMaterial(
             new THREE.Vector2(this.canvas.width, this.canvas.height),
             result.value,
@@ -77,6 +146,8 @@ export default class WebGLScene {
               radius: el.radius,
             }))
           );
+          material.needsUpdate = true;
+          this.sceneObjects[key].material = material;
           const image = new THREE.Mesh(planeGeometry, material);
           const imagePosition = objectSettings[i].position;
           image.position.set(imagePosition.x, imagePosition.y, imagePosition.z);
@@ -107,6 +178,7 @@ export default class WebGLScene {
       this.cameraPosition.z
     );
     this.render();
+    this.startAnimation();
   }
 
   setColor() {
@@ -126,5 +198,56 @@ export default class WebGLScene {
     this.camera.updateProjectionMatrix();
     this.renderer.setSize(this.canvas.width, this.canvas.height);
     this.render();
+  }
+
+  startAnimation() {
+    if (!this.canvas) {
+      return;
+    }
+
+    this.stopAnimation();
+    this.tick();
+  }
+
+  tick() {
+    const now = Date.now();
+    const elapsed = now - this.lastFrameTime;
+
+    this.runnungAnimation = requestAnimationFrame(() => this.tick());
+
+    if (elapsed > this.frameInterval) {
+      this.lastFrameTime = now - (elapsed % this.frameInterval);
+      const currentSceneObject = this.sceneObjects[this.currentSceneObject];
+      currentSceneObject.animationFunctions.forEach((animationFunction, index) => {
+        const isAnimationDelayed = now < this.startTime + currentSceneObject.delays[index];
+        const isAnimationFinished = currentSceneObject.finites[index]
+          ? now > this.startTime + currentSceneObject.delays[index] + currentSceneObject.durations[index]
+          : false;
+        if (!isAnimationDelayed && !isAnimationFinished) {
+          const pastProgress = (now - this.startTime - currentSceneObject.delays[index]) / currentSceneObject.durations[index];
+          const progress = currentSceneObject.finites[index] ? pastProgress : pastProgress - Math.trunc(pastProgress);
+          animationFunction(progress);
+          this.render();
+        }
+      });
+    }
+  }
+
+  stopAnimation() {
+    if (this.runnungAnimation) {
+      cancelAnimationFrame(this.runnungAnimation);
+      this.runnungAnimation = this.startTime = this.lastFrameTime = null;
+    }
+  }
+
+  hueBlinkAnimationFunct(progress) {
+    const hueForProgress = 6 * Math.sin(6.3 * progress + 1.6) - 6 + Math.random();
+    const hueRad = THREE.Math.degToRad(hueForProgress);
+    this.sceneObjects[this.currentSceneObject].material.uniforms.hue.value = hueRad;
+    this.sceneObjects[this.currentSceneObject].material.needsUpdate = true;
+  }
+
+  bubblesMoveAnimationFunct(progress) {
+
   }
 }
