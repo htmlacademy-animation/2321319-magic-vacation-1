474cf5fbaa61bb5a47353a31618dfc0ed17e0d59
Keks <keks@htmlacademy.ru>
2023-06-16 08:17:36 +0300

    :heavy_check_mark: Сборка #32

--- 32/js/script.js
+++ 32/js/script.js
@@ -68690,60 +68690,60 @@ class IntroScene extends _default_scene__WEBPACK_IMPORTED_MODULE_3__["default"] 
   }
 
   initAnimationsSettings() {
-    const delayForAnimation = 900;
+    const delayForAnimation = 1000;
     this.animationObjects = {
       [_general_consts__WEBPACK_IMPORTED_MODULE_1__["AnimatedPrimitives"].CHANDELIER]: {
-        durations: [800, 4000],
+        durations: [800, 2500],
         finites: [true, false],
-        delays: [1200, delayForAnimation + 1300],
+        delays: [1200, delayForAnimation + 380],
         animationFunctions: [
           (el, progress) => this.chandelierAnimationFunc(el, progress),
-          (el, progress) => this.elementMoveAnimationFunc(el, progress, 15),
+          (el, progress) => this.elementMoveAnimationFunc(el, progress, 1.35),
         ],
       },
       [_general_consts__WEBPACK_IMPORTED_MODULE_1__["SvgShape"].FLAMINGO.id]: {
-        durations: [800, 4000],
+        durations: [800, 2500],
         finites: [true, false],
-        delays: [1200, delayForAnimation + 1100],
+        delays: [1200, delayForAnimation + 250],
         animationFunctions: [
           (el, progress) => this.flamingoAnimationFunc(el, progress),
-          (el, progress) => this.elementMoveAnimationFunc(el, progress, 9),
+          (el, progress) => this.elementMoveAnimationFunc(el, progress, 1.2),
         ],
       },
       [_general_consts__WEBPACK_IMPORTED_MODULE_1__["SvgShape"].SNOWFLAKE.id]: {
-        durations: [800, 4000],
+        durations: [800, 2500],
         finites: [true, false],
-        delays: [1200, delayForAnimation + 900],
+        delays: [1200, delayForAnimation + 100],
         animationFunctions: [
           (el, progress) => this.snowflakeAnimationFunc(el, progress),
-          (el, progress) => this.elementMoveAnimationFunc(el, progress, 11),
+          (el, progress) => this.elementMoveAnimationFunc(el, progress, 1.1),
         ],
       },
       [_general_consts__WEBPACK_IMPORTED_MODULE_1__["SvgShape"].QUESTION.id]: {
-        durations: [800, 4000],
+        durations: [800, 2500],
         finites: [true, false],
-        delays: [1200, delayForAnimation + 1500],
+        delays: [1200, delayForAnimation + 400],
         animationFunctions: [
           (el, progress) => this.questionAnimationFunc(el, progress),
-          (el, progress) => this.elementMoveAnimationFunc(el, progress, 5),
+          (el, progress) => this.elementMoveAnimationFunc(el, progress, 1),
         ],
       },
       [_general_consts__WEBPACK_IMPORTED_MODULE_1__["SvgShape"].LEAF.id]: {
-        durations: [800, 4000],
+        durations: [800, 2500],
         finites: [true, false],
-        delays: [1200, delayForAnimation + 1600],
+        delays: [1200, delayForAnimation + 500],
         animationFunctions: [
           (el, progress) => this.leafAnimationFunc(el, progress),
-          (el, progress) => this.elementMoveAnimationFunc(el, progress, 10),
+          (el, progress) => this.elementMoveAnimationFunc(el, progress, 1.25),
         ],
       },
       [_general_consts__WEBPACK_IMPORTED_MODULE_1__["Objects"].WATERMELON.id]: {
-        durations: [800, 4000],
+        durations: [800, 2500],
         finites: [true, false],
-        delays: [1200, delayForAnimation + 1700],
+        delays: [1200, delayForAnimation + 650],
         animationFunctions: [
           (el, progress) => this.watermelonAnimationFunc(el, progress),
-          (el, progress) => this.elementMoveAnimationFunc(el, progress, 13),
+          (el, progress) => this.elementMoveAnimationFunc(el, progress, 1.3),
         ],
       }
     };
@@ -68751,13 +68751,13 @@ class IntroScene extends _default_scene__WEBPACK_IMPORTED_MODULE_3__["default"] 
 
   chandelierAnimationFunc(element, progress) {
     // from 5, -20, 25 to 485 -140 105
-    const x = 5 + 480 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutExpo"])(progress);
-    const y = -20 - 120 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutExpo"])(progress);
-    const z = 25 + 80 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutExpo"])(progress);
+    const x = 5 + 480 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutQuad"])(progress);
+    const y = -20 - 120 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutQuad"])(progress);
+    const z = 25 + 80 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutQuad"])(progress);
     // from 0 0 0 to 0.7, 0.7, 0.7
-    const scale = 0.7 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutExpo"])(progress);
+    const scale = 0.7 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutQuad"])(progress);
     // from 10 0 -60 to 10, 0, 15
-    const zRotate = -60 + 75 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutExpo"])(progress);
+    const zRotate = -60 + 75 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutQuad"])(progress);
     this.setPosition(element.element, [x, y, z], [scale, scale, scale], [10, 0, zRotate]);
   }
 
@@ -68770,61 +68770,61 @@ class IntroScene extends _default_scene__WEBPACK_IMPORTED_MODULE_3__["default"] 
 
   flamingoAnimationFunc(element, progress) {
     // from -50, 30, 20 to -510, 384, 80
-    const x = -50 - 460 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutExpo"])(progress);
-    const y = 30 + 354 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutExpo"])(progress);
-    const z = 20 + 60 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutExpo"])(progress);
+    const x = -50 - 460 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutQuad"])(progress);
+    const y = 30 + 354 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutQuad"])(progress);
+    const z = 20 + 60 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutQuad"])(progress);
     // from 0 0 0 to 0.8, 0.8, 0.8
-    const scale = 0.8 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutExpo"])(progress);
+    const scale = 0.8 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutQuad"])(progress);
     // from -10 60 205 to -10, 30, 205
-    const yRotate = 60 - 30 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutExpo"])(progress);
+    const yRotate = 60 - 30 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutQuad"])(progress);
     this.setPosition(element.element, [x, y, z], [scale, scale, scale], [-10, yRotate, 205]);
   }
 
   snowflakeAnimationFunc(element, progress) {
     // from -40, -20, 20 to -395, 100, 70
-    const x = -40 - 355 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutExpo"])(progress);
-    const y = -20 + 120 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutExpo"])(progress);
-    const z = 20 + 50 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutExpo"])(progress);
+    const x = -40 - 355 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutQuad"])(progress);
+    const y = -20 + 120 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutQuad"])(progress);
+    const z = 20 + 50 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutQuad"])(progress);
     // from 0 0 0 to 1.4, 1.4, 1.4
-    const scale = 1.4 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutExpo"])(progress);
+    const scale = 1.4 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutQuad"])(progress);
     // from -20 70 200 to -20, 40, 200
-    const yRotate = 70 - 30 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutExpo"])(progress);
+    const yRotate = 70 - 30 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutQuad"])(progress);
     this.setPosition(element.element, [x, y, z], [scale, scale, scale], [-20, yRotate, 200]);
   }
 
   questionAnimationFunc(element, progress) {
     // from 10, -60, 20 to -115, -345, 65
-    const x = 20 - 135 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutExpo"])(progress);
-    const y = -60 - 285 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutExpo"])(progress);
-    const z = 20 + 45 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutExpo"])(progress);
+    const x = 20 - 135 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutQuad"])(progress);
+    const y = -60 - 285 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutQuad"])(progress);
+    const z = 20 + 45 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutQuad"])(progress);
     // from 0 0 0 to 1.2, 1.2, 1.2
-    const scale = 1.2 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutExpo"])(progress);
+    const scale = 1.2 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutQuad"])(progress);
     // from -45, 180, 210 to -45, 180, 160
-    const zRotate = 210 - 50 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutExpo"])(progress);
+    const zRotate = 210 - 50 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutQuad"])(progress);
     this.setPosition(element.element, [x, y, z], [scale, scale, scale], [-45, 180, zRotate]);
   }
 
   leafAnimationFunc(element, progress) {
     // from 7, 40, 20 to 690, 360, 145
-    const x = 7 + 683 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutExpo"])(progress);
-    const y = 40 + 320 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutExpo"])(progress);
-    const z = 20 + 125 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutExpo"])(progress);
+    const x = 7 + 683 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutQuad"])(progress);
+    const y = 40 + 320 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutQuad"])(progress);
+    const z = 20 + 125 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutQuad"])(progress);
     // from 0 0 0 to 1.8, 1.8, 1.8
-    const scale = 1.8 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutExpo"])(progress);
+    const scale = 1.8 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutQuad"])(progress);
     // from 5, 90, 245 to 5, 135, 245
-    const yRotate = 90 + 45 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutExpo"])(progress);
+    const yRotate = 90 + 45 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutQuad"])(progress);
     this.setPosition(element.element, [x, y, z], [scale, scale, scale], [5, yRotate, 245]);
   }
 
   watermelonAnimationFunc(element, progress) {
     // from -60, -70, 20 to -750, -265, 120
-    const x = -60 - 690 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutExpo"])(progress);
-    const y = -70 - 195 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutExpo"])(progress);
-    const z = 20 + 100 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutExpo"])(progress);
+    const x = -60 - 690 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutQuad"])(progress);
+    const y = -70 - 195 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutQuad"])(progress);
+    const z = 20 + 100 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutQuad"])(progress);
     // from 0 0 0 to 2.5, 2.5, 2.5
-    const scale = 2.5 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutExpo"])(progress);
+    const scale = 2.5 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutQuad"])(progress);
     // from 15, -10, 210 to 15, -10, 140
-    const zRotate = 210 - 70 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutExpo"])(progress);
+    const zRotate = 210 - 70 * Object(_general_easing__WEBPACK_IMPORTED_MODULE_2__["easeOutQuad"])(progress);
     this.setPosition(element.element, [x, y, z], [scale, scale, scale], [15, -10, zRotate]);
   }
 }
@@ -71031,7 +71031,7 @@ __webpack_require__.r(__webpack_exports__);
 
 class WebGLScene extends _canvas_animation__WEBPACK_IMPORTED_MODULE_9__["default"] {
   constructor(canvasElement) {
-    super(canvasElement, 100, _general_consts__WEBPACK_IMPORTED_MODULE_2__["AnimationType"]._3D);
+    super(canvasElement, 60, _general_consts__WEBPACK_IMPORTED_MODULE_2__["AnimationType"]._3D);
     super.setSizes();
     this.aspectRatio = this.canvas.width / this.canvas.height;
     this.storyScenes = null;
