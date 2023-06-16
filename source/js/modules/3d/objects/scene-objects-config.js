d4db9de3157be9d230054e59677add115da4e1ed
tadian-solo <tadian-solo@yandex.ru>
2023-06-16 15:36:46 +0300

    Анимация появления объекта «Чемодан» в комнате 1 «Истории»

--- source/js/modules/3d/objects/scene-objects-config.js
+++ source/js/modules/3d/objects/scene-objects-config.js
@@ -39,137 +39,11 @@ export const SceneObjects = {
       //     },
       //   ],
       // },
-      {
-        id: AnimatedPrimitives.CHANDELIER,
-        position: [5, -20, 25],
-        scale: [0, 0, 0],
-        rotation: [10, 0, -60],
-        children: [
-          {
-            id: `chandelierLathe`,
-            primitiveType: `LatheGeometry`,
-            primitiveSettings: [
-              getLathePointsBy(80, 40, 2).map((el) => new THREE.Vector2(...el)),
-              35,
-              0,
-              2 * Math.PI
-            ],
-            position: [0, 0, 0],
-            scale: [1, 1, 1],
-            rotation: [0, 0, 18],
-            materialType: MaterialType.SOFT.id,
-            materialProps: {
-              color: ObjectColor.BRIGHT_PURPLE.id,
-            }
-          },
-          {
-            id: `chandelierBody`,
-            primitiveType: `SphereGeometry`,
-            primitiveSettings: [60, 80, 80],
-            position: [0, 0, 0],
-            scale: [1, 1, 1],
-            rotation: [0, 0, 0],
-            materialType: MaterialType.SOFT.id,
-            materialProps: {
-              color: ObjectColor.DOMINANT_RED.id,
-            }
-          },
-        ]
-      }
-    ],
-    svgShapes: [
-      {
-        id: SvgShape.FLAMINGO.id,
-        position: [-50, 30, 20],
-        scale: [0, 0, 0],
-        rotation: [-10, 60, 205],
-        extrudeSettings: { depth: 12, bevelOffset: -2 },
-        materialType: MaterialType.SOFT.id,
-        materialProps: {
-          color: ObjectColor.LIGHT_DOMINANT_RED.id,
-        }
-      },
-      {
-        id: SvgShape.SNOWFLAKE.id,
-        position: [-40, -20, 20],
-        scale: [0, 0, 0],
-        rotation: [-20, 70, 200],
-        extrudeSettings: { depth: 11, bevelOffset: -2 },
-        materialType: MaterialType.BASIC.id,
-        materialProps: {
-          color: ObjectColor.BLUE.id,
-        }
-      },
-      {
-        id: SvgShape.QUESTION.id,
-        position: [10, -60, 20],
-        scale: [0, 0, 0],
-        rotation: [-45, 180, 210],
-        extrudeSettings: { depth: 14, bevelOffset: -2 },
-        materialType: MaterialType.BASIC.id,
-        materialProps: {
-          color: ObjectColor.BLUE.id,
-        }
-      },
-      {
-        id: SvgShape.KEYHOLE.id,
-        position: [1000, 1000, 0],
-        scale: [1.02, 1.02, 1.02],
-        rotation: [0, 0, 180],
-        extrudeSettings: { depth: 20, bevelOffset: -2 },
-        materialType: MaterialType.SOFT.id,
-        materialProps: {
-          color: ObjectColor.DARK_PURPLE.id,
-        }
-      },
-      {
-        id: SvgShape.LEAF.id,
-        position: [7, 40, 20],
-        scale: [0, 0, 0],
-        rotation: [5, 90, 245],
-        extrudeSettings: { depth: 14, bevelOffset: -2 },
-        materialType: MaterialType.BASIC.id,
-        materialProps: {
-          color: ObjectColor.GREEN.id,
-        }
-      },
-    ],
-    objects: [
-      {
-        id: Objects.AIRPLANE.id,
-        position: [275, 140, 140],
-        scale: [1.45, 1.45, 1.45],
-        rotation: [60, 140, -15],
-        materialType: MaterialType.BASIC.id,
-        materialProps: {
-          color: ObjectColor.WHITE.id,
-        }
-      },
-      {
-        id: Objects.WATERMELON.id,
-        position: [-60, -70, 20],
-        scale: [0, 0, 0],
-        rotation: [15, -10, 210],
-      },
-      {
-        id: Objects.SUITCASE.id,
-        position: [-75, -195, 95],
-        scale: [0.55, 0.55, 0.55],
-        rotation: [30, 215, 15],
-      },
-    ],
-  },
-  [ThemeColor.LIGHT_PURPLE]: {
-    backgroundImage: `./img/module-5/scenes-textures/scene-1.png`,
-    position: [0, 400, 0],
-    scale: [1, 1, 1],
-    rotation: [0, 0, 0],
-    primitives: [
       // {
-      //   id: `chandelier`,
-      //   position: [30, 130, 600],
-      //   scale: [0.9, 0.9, 0.9],
-      //   rotation: [0, 0, 0],
+      //   id: AnimatedPrimitives.CHANDELIER,
+      //   position: [5, -20, 25],
+      //   scale: [0, 0, 0],
+      //   rotation: [10, 0, -60],
       //   children: [
       //     {
       //       id: `chandelierLathe`,
@@ -200,122 +74,255 @@ export const SceneObjects = {
       //         color: ObjectColor.DOMINANT_RED.id,
       //       }
       //     },
-      //     {
-      //       id: `chandelierThread`,
-      //       primitiveType: `CylinderGeometry`,
-      //       primitiveSettings: [1, 1, 1000],
-      //       position: [0, 560, 0],
-      //       scale: [1, 1, 1],
-      //       rotation: [0, 0, 0],
-      //       materialType: MaterialType.SOFT.id,
-      //       materialProps: {
-      //         color: ObjectColor.METAL_GREY.id,
-      //       }
-      //     },
-      //     {
-      //       id: `chandelierHanging`,
-      //       primitiveType: `SphereGeometry`,
-      //       primitiveSettings: [10, 80, 80],
-      //       position: [0, 120, 0],
-      //       scale: [1, 1, 1],
-      //       rotation: [0, 0, 0],
-      //       materialType: MaterialType.SOFT.id,
-      //       materialProps: {
-      //         color: ObjectColor.BRIGHT_PURPLE.id,
-      //       }
-      //     },
-      //   ],
-      // },
-      // {
-      //   id: `carpet`,
-      //   position: [0, -495, 80],
-      //   scale: [0.9, 0.9, 0.9],
-      //   rotation: [0, -55, 0],
-      //   children: [
-      //     {
-      //       id: `carpetLathe`,
-      //       primitiveType: `LatheGeometry`,
-      //       primitiveSettings: [
-      //         getLathePointsBy(763, 180, 3).map((el) => new THREE.Vector2(...el)),
-      //         12,
-      //         THREE.Math.degToRad(16),
-      //         THREE.Math.degToRad(74)
-      //       ],
-      //       position: [0, 0, 0],
-      //       scale: [1, 1, 1],
-      //       rotation: [0, 0, 0],
-      //       materialType: MaterialType.CUSTOM.id,
-      //       materialProps: {
-      //         materialConstructor: CarpetMaterial,
-      //         mainColor: ObjectColor.LIGHT_PURPLE.id,
-      //         secondaryColor: ObjectColor.ADDITIONAL_PURPLE.id,
-      //         textureFrequency: 3.5
-      //       }
-      //     }
-      //   ],
-      // },
-      // {
-      //   id: `floor`,
-      //   position: [0, 0, 0],
-      //   scale: [1, 1, 1],
-      //   rotation: [0, 0, 0],
-      //   children: [
-      //     {
-      //       id: `floorBody`,
-      //       primitiveType: `CircleGeometry`,
-      //       primitiveSettings: [1350, 60, 0, Math.PI / 2],
-      //       position: [0, -500, 0],
-      //       scale: [1, 1, 1],
-      //       rotation: [90, 0, 45],
-      //       materialType: MaterialType.SOFT.id,
-      //       materialProps: {
-      //         color: ObjectColor.DARK_PURPLE.id,
-      //         side: THREE.DoubleSide
-      //       }
-      //     }
       //   ]
       // }
     ],
     svgShapes: [
       // {
-      //   id: SvgShape.FLOWER.id,
-      //   position: [-240, -108, 340],
-      //   scale: [0.6, 0.6, 0.6],
-      //   rotation: [180, -45, 0],
-      //   extrudeSettings: { depth: 4, bevelOffset: -2 },
+      //   id: SvgShape.FLAMINGO.id,
+      //   position: [-50, 30, 20],
+      //   scale: [0, 0, 0],
+      //   rotation: [-10, 60, 205],
+      //   extrudeSettings: { depth: 12, bevelOffset: -2 },
+      //   materialType: MaterialType.SOFT.id,
+      //   materialProps: {
+      //     color: ObjectColor.LIGHT_DOMINANT_RED.id,
+      //   }
+      // },
+      // {
+      //   id: SvgShape.SNOWFLAKE.id,
+      //   position: [-40, -20, 20],
+      //   scale: [0, 0, 0],
+      //   rotation: [-20, 70, 200],
+      //   extrudeSettings: { depth: 11, bevelOffset: -2 },
       //   materialType: MaterialType.BASIC.id,
       //   materialProps: {
+      //     color: ObjectColor.BLUE.id,
+      //   }
+      // },
+      // {
+      //   id: SvgShape.QUESTION.id,
+      //   position: [10, -60, 20],
+      //   scale: [0, 0, 0],
+      //   rotation: [-45, 180, 210],
+      //   extrudeSettings: { depth: 14, bevelOffset: -2 },
+      //   materialType: MaterialType.BASIC.id,
+      //   materialProps: {
+      //     color: ObjectColor.BLUE.id,
+      //   }
+      // },
+      // {
+      //   id: SvgShape.KEYHOLE.id,
+      //   position: [1000, 1000, 0],
+      //   scale: [1.02, 1.02, 1.02],
+      //   rotation: [0, 0, 180],
+      //   extrudeSettings: { depth: 20, bevelOffset: -2 },
+      //   materialType: MaterialType.SOFT.id,
+      //   materialProps: {
       //     color: ObjectColor.DARK_PURPLE.id,
-      //     side: THREE.DoubleSide,
+      //   }
+      // },
+      // {
+      //   id: SvgShape.LEAF.id,
+      //   position: [7, 40, 20],
+      //   scale: [0, 0, 0],
+      //   rotation: [5, 90, 245],
+      //   extrudeSettings: { depth: 14, bevelOffset: -2 },
+      //   materialType: MaterialType.BASIC.id,
+      //   materialProps: {
+      //     color: ObjectColor.GREEN.id,
       //   }
       // },
     ],
     objects: [
       // {
-      //   id: Objects.WALLS.id,
-      //   position: [0, -500, 0],
-      //   scale: [1, 1, 1],
-      //   rotation: [0, -45, 0],
-      //   materialType: MaterialType.SOFT.id,
+      //   id: Objects.AIRPLANE.id,
+      //   position: [275, 140, 140],
+      //   scale: [1.45, 1.45, 1.45],
+      //   rotation: [60, 140, -15],
+      //   materialType: MaterialType.BASIC.id,
       //   materialProps: {
-      //     color: ObjectColor.PURPLE.id,
-      //     side: THREE.DoubleSide
+      //     color: ObjectColor.WHITE.id,
       //   }
       // },
       // {
-      //   id: Objects.ROOM_1_STATIC.id,
-      //   position: [-5, -495, 5],
-      //   scale: [1, 1, 1],
-      //   rotation: [0, -45, 0]
+      //   id: Objects.WATERMELON.id,
+      //   position: [-60, -70, 20],
+      //   scale: [0, 0, 0],
+      //   rotation: [15, -10, 210],
+      // },
+      // {
+      //   id: Objects.SUITCASE.id,
+      //   position: [-75, -195, 95],
+      //   scale: [0.55, 0.55, 0.55],
+      //   rotation: [30, 215, 15],
       // },
     ],
+    animatedObjects: []
+  },
+  [ThemeColor.LIGHT_PURPLE]: {
+    backgroundImage: `./img/module-5/scenes-textures/scene-1.png`,
+    position: [0, 400, 0],
+    scale: [1, 1, 1],
+    rotation: [0, 0, 0],
+    primitives: [
+      {
+        id: `chandelier`,
+        position: [30, 130, 600],
+        scale: [0.9, 0.9, 0.9],
+        rotation: [0, 0, 0],
+        children: [
+          {
+            id: `chandelierLathe`,
+            primitiveType: `LatheGeometry`,
+            primitiveSettings: [
+              getLathePointsBy(80, 40, 2).map((el) => new THREE.Vector2(...el)),
+              35,
+              0,
+              2 * Math.PI
+            ],
+            position: [0, 0, 0],
+            scale: [1, 1, 1],
+            rotation: [0, 0, 18],
+            materialType: MaterialType.SOFT.id,
+            materialProps: {
+              color: ObjectColor.BRIGHT_PURPLE.id,
+            }
+          },
+          {
+            id: `chandelierBody`,
+            primitiveType: `SphereGeometry`,
+            primitiveSettings: [60, 80, 80],
+            position: [0, 0, 0],
+            scale: [1, 1, 1],
+            rotation: [0, 0, 0],
+            materialType: MaterialType.SOFT.id,
+            materialProps: {
+              color: ObjectColor.DOMINANT_RED.id,
+            }
+          },
+          {
+            id: `chandelierThread`,
+            primitiveType: `CylinderGeometry`,
+            primitiveSettings: [1, 1, 1000],
+            position: [0, 560, 0],
+            scale: [1, 1, 1],
+            rotation: [0, 0, 0],
+            materialType: MaterialType.SOFT.id,
+            materialProps: {
+              color: ObjectColor.METAL_GREY.id,
+            }
+          },
+          {
+            id: `chandelierHanging`,
+            primitiveType: `SphereGeometry`,
+            primitiveSettings: [10, 80, 80],
+            position: [0, 120, 0],
+            scale: [1, 1, 1],
+            rotation: [0, 0, 0],
+            materialType: MaterialType.SOFT.id,
+            materialProps: {
+              color: ObjectColor.BRIGHT_PURPLE.id,
+            }
+          },
+        ],
+      },
+      {
+        id: `carpet`,
+        position: [0, -495, 80],
+        scale: [0.9, 0.9, 0.9],
+        rotation: [0, -55, 0],
+        children: [
+          {
+            id: `carpetLathe`,
+            primitiveType: `LatheGeometry`,
+            primitiveSettings: [
+              getLathePointsBy(763, 180, 3).map((el) => new THREE.Vector2(...el)),
+              12,
+              THREE.Math.degToRad(16),
+              THREE.Math.degToRad(74)
+            ],
+            position: [0, 0, 0],
+            scale: [1, 1, 1],
+            rotation: [0, 0, 0],
+            materialType: MaterialType.CUSTOM.id,
+            materialProps: {
+              materialConstructor: CarpetMaterial,
+              mainColor: ObjectColor.LIGHT_PURPLE.id,
+              secondaryColor: ObjectColor.ADDITIONAL_PURPLE.id,
+              textureFrequency: 3.5
+            }
+          }
+        ],
+      },
+      {
+        id: `floor`,
+        position: [0, 0, 0],
+        scale: [1, 1, 1],
+        rotation: [0, 0, 0],
+        children: [
+          {
+            id: `floorBody`,
+            primitiveType: `CircleGeometry`,
+            primitiveSettings: [1350, 60, 0, Math.PI / 2],
+            position: [0, -500, 0],
+            scale: [1, 1, 1],
+            rotation: [90, 0, 45],
+            materialType: MaterialType.SOFT.id,
+            materialProps: {
+              color: ObjectColor.DARK_PURPLE.id,
+              side: THREE.DoubleSide
+            }
+          }
+        ]
+      }
+    ],
+    svgShapes: [
+      {
+        id: SvgShape.FLOWER.id,
+        position: [-240, -108, 340],
+        scale: [0.6, 0.6, 0.6],
+        rotation: [180, -45, 0],
+        extrudeSettings: { depth: 4, bevelOffset: -2 },
+        materialType: MaterialType.BASIC.id,
+        materialProps: {
+          color: ObjectColor.DARK_PURPLE.id,
+          side: THREE.DoubleSide,
+        }
+      },
+    ],
+    objects: [
+      {
+        id: Objects.WALLS.id,
+        position: [0, -500, 0],
+        scale: [1, 1, 1],
+        rotation: [0, -45, 0],
+        materialType: MaterialType.SOFT.id,
+        materialProps: {
+          color: ObjectColor.PURPLE.id,
+          side: THREE.DoubleSide
+        }
+      },
+      {
+        id: Objects.ROOM_1_STATIC.id,
+        position: [-5, -495, 5],
+        scale: [1, 1, 1],
+        rotation: [0, -45, 0]
+      },
+    ],
     animatedObjects: [
-    //   {
-    //     id: Objects.DOG.id,
-    //     position: [50, -490, 620],
-    //     scale: [1, 1, 1],
-    //     rotation: [0, 20, 0],
-    //   },
+      {
+        id: Objects.DOG.id,
+        position: [50, -490, 620],
+        scale: [1, 1, 1],
+        rotation: [0, 20, 0],
+      },
+      {
+        id: Objects.SUITCASE.id,
+        position: [-336, -380, 756],
+        scale: [0, 0, 0],
+        rotation: [0, 0, 0],
+      },
     ]
   },
   [ThemeColor.BLUE]: {
@@ -499,6 +506,7 @@ export const SceneObjects = {
       //   ],
       // },
     ],
+    animatedObjects: []
   },
   [ThemeColor.LIGHT_BLUE]: {
     backgroundImage: `./img/module-5/scenes-textures/scene-3.png`,
--- source/js/modules/3d/scenes/story-dog-scene.js
+++ source/js/modules/3d/scenes/story-dog-scene.js
@@ -7,12 +7,28 @@ export default class DogScene extends DefaultScene {
   constructor(objectLoader) {
     super(objectLoader);
     this.sceneId = ThemeColor.LIGHT_PURPLE;
+    this.suitcaseStartPosition = [-336, -380, 756];
+    this.initAnimationsSettings();
   }
 
   initObjects() {
     super.initObjects();
     this.addDog();
-    // this.addSuitcase();
+    this.addSuitcase();
+  }
+
+  initAnimationsSettings() {
+    this.animationObjects = {
+      [Objects.SUITCASE.id]: {
+        durations: [10, 800],
+        finites: [true, true],
+        delays: [400, 500],
+        animationFunctions: [
+          (el, progress) => this.suitcaseShowAnimationFunc(el, progress),
+          (el, progress) => this.suitcaseAppearenceAnimationFunc(el, progress),
+        ],
+      },
+    };
   }
 
   addDog() {
@@ -20,19 +36,51 @@ export default class DogScene extends DefaultScene {
   }
 
   addSuitcase() {
-    const group = new THREE.Group();
+    this.suitcase = new THREE.Group();
     const object = this.objectLoader
       .getPreparedObjectWithMateral(Objects.SUITCASE.id)
       .clone();
     this.setPosition(object, [0, 0, 0], [1, 1, 1], [0, -45, 0]);
-    group.add(object);
-    this.setPosition(group, [-336, -500, 756], [1, 1, 1], [0, 0, 0]);
-    this.sceneGroup.add(group);
+    this.suitcase.add(object);
+    this.setPosition(
+      this.suitcase,
+      this.suitcaseStartPosition,
+      [0, 0, 0],
+      [0, 0, 0]
+    );
+    this.sceneGroup.add(this.suitcase);
+
+    const modelBoundingBox = new THREE.Box3().setFromObject(object);
+    this.suitcaseYSize = modelBoundingBox.max.y - modelBoundingBox.min.y;
 
     // const geometry = new THREE.CircleGeometry(840, 32);
     // const material = new THREE.MeshBasicMaterial({ color: 0xffff00, side: THREE.DoubleSide });
     // const circle = new THREE.Mesh(geometry, material);
     // this.setPosition(circle, [0, -490, 0], [1, 1, 1], [90, 0, 45]);
     // this.sceneGroup.add(circle);
+  }
+
+  suitcaseShowAnimationFunc(_el, _progress) {
+    this.setPosition(this.suitcase, this.suitcaseStartPosition, [1, 1, 1], [0, 0, 0]);
+  }
+
+  suitcaseAppearenceAnimationFunc(_el, progress) {
+    let y = this.suitcase.position.y;
+    let scale = [1, 1, 1];
+
+    const yScale =
+      Math.pow(1 - progress, 3) +
+      3 * Math.pow(1 - progress, 2) * progress * 1.4 +
+      3 * (1 - progress) * Math.pow(progress, 2) * 0.6 +
+      Math.pow(progress, 3);
+    const diff = (yScale - 1) / 2;
+    scale = [1 - diff, yScale, 1 - diff];
+    const scaled = yScale * this.suitcaseYSize - this.suitcaseYSize;
+
+    if (progress <= 0.65) {
+      y = -380 - 160 * progress - scaled;
+    }
+
+    this.setPosition(this.suitcase, [-336, y, 756], scale, [0, 0, 0]);
   }
 }
